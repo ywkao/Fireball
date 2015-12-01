1838891d96b75604f9336454fb24c133d49132a2
ywkao <ywkao@hep1.phys.ntu.edu.tw>
2015-12-01 23:51:06 +0100

    update

--- workspace/Copy.sh
+++ workspace/Copy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 Dir1=/afs/cern.ch/user/y/ykao/work/MG5_aMC_v2_2_3/Delphes/workspace
 Dir2=/afs/cern.ch/user/y/ykao/work/MG5_aMC_v2_2_3/Delphes/workspace_ABCD
-cp -ar ${Dir1} ${Dir2} ~/work/fireball/
 cp -ar ${Dir1} ${Dir2} ~/work/Fireball/
 ls -lhrt ~/work/Fireball/workspace/
 ls -lhrt ~/work/Fireball/workspace_ABCD/
